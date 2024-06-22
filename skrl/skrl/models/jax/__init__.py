5042d0e8cc8639455446c19138efed6a52a55902
Chao-Chen-Hao <a0922866854@gmail.com>
2024-06-22 20:56:10 +0800

    Clone Toni-SM/skrl (at commit 631613a) Reference: https://github.com/Toni-SM/skrl

---
+++ skrl/skrl/models/jax/__init__.py
@@ -0,0 +1,6 @@
+from skrl.models.jax.base import Model  # isort:skip
+
+from skrl.models.jax.categorical import CategoricalMixin
+from skrl.models.jax.deterministic import DeterministicMixin
+from skrl.models.jax.gaussian import GaussianMixin
+from skrl.models.jax.multicategorical import MultiCategoricalMixin
